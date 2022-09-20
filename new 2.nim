1f657d76fa1b365523ce8b96f1cd374ba5e69c10
kimmickal <65750233+kimmickal@users.noreply.github.com>
2022-09-20 17:08:45 -0500

    Add files via upload
    Keeping track of family members.

---
+++ new 2.nim
@@ -0,0 +1,13 @@
+CREATE TABLE family (
+   id INTEGER,
+   name TEXT,
+   age INTEGER
+);
+
+INSERT INTO family (id, name, age);
+VALUES (1, 'Randy Kimmick' , 71);
+INSERT INTO family (id, name, age);
+VALUES (2, 'Laura Kimmick', 58);
+INSERT INTO family (id, name, age);
+VALUES (3, 'Kobe Kimmick', 20);
+
